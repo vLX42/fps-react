25eabf42c9f693491c4d7c4975b538bce07994fa
Peter Biro <github@vlx.dk>
2021-01-25 21:49:21 +0100

    Changes to test and readme

--- test/fps.test.tsx
+++ test/fps.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Default as FPSStats } from '../stories/FPSStats.stories';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<FPSStats />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
